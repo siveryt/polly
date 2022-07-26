17423f90e75a04ad788fc30269f20a2323472044
Sivery <github@sivery.de>
2022-07-26 10:12:06 +0200

    Reactivated and added Command and Modal for creating Poll

--- README.md
+++ README.md
@@ -4,5 +4,3 @@
 
 Many Discord-Sevrer nowadays use Polls to interact with their community, but usually these endup in Messages and Readtions attached to them and no time limit. Polly solves this Issue with Button and Time-restricted polls.
 The many options give you the opportunity to customize your polls to your needs
-
-Discontinued because of a bulky interface. I originally planned to do this with modals, but it didn't work out like I expected it to come out. Discord Modals can only have 5 components
--- commands/poll.js
+++ commands/poll.js
@@ -1,10 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const {
-    Modal,
-    TextInputComponent,
-    SelectMenuComponent,
-    showModal,
-} = require('discord-modals'); // Import all
+const { Modal, TextInputComponent, showModal } = require('discord-modals'); // Import all
 const discordModals = require('discord-modals'); // Define the discord-modals package!
 
 module.exports = {
@@ -17,77 +12,28 @@ module.exports = {
             .setName('options')
             .setDescription('How many options should the poll have?')
             .setRequired(true)
+            .setMaxValue(5)
+            .setMinValue(2)
+        )
+        .addStringOption((option) =>
+            option
+            .setName('question')
+            .setDescription("What's the question?")
+            .setRequired(true)
+        )
+        .addNumberOption((option) =>
+            option.setName('time').setDescription('How long should the poll last?')
         ),
     async execute(interaction, client) {
         discordModals(client);
 
         const modal = new Modal() // We create a Modal
             .setCustomId('modal-customid')
-            .setTitle('Modal')
-            .addComponents(
-                new TextInputComponent() // We create a Text Input Component
-                .setCustomId('title')
-                .setLabel('Title')
-                .setStyle('SHORT') // IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
-                .setPlaceholder('How should the poll be called?')
-                .setRequired(true), // If it's required or not
-
-                new TextInputComponent() // We create a Text Input Component
-                .setCustomId('description')
-                .setLabel('Description')
-                .setStyle('LONG') // IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
-                .setPlaceholder('Descripe your poll')
-                .setRequired(false), // If it's required or not
-
-                new SelectMenuComponent() // We create a Select Menu Component
-                .setCustomId('expire')
-                .setPlaceholder('When should the poll expire?')
-                .addOptions({
-                    label: '1m',
-                    value: '1',
-                }, {
-                    label: '2m',
-                    value: '2',
-                }, {
-                    label: '3m',
-                    value: '3',
-                }, {
-                    label: '5m',
-                    value: '5',
-                }, {
-                    label: '10m',
-                    value: '10',
-                }, {
-                    label: '30m',
-                    value: '30',
-                }, {
-                    label: '1h',
-                    value: '60',
-                }, {
-                    label: '2h',
-                    value: '120',
-                }, {
-                    label: '5h',
-                    value: '300',
-                }, {
-                    label: '12h',
-                    value: '720',
-                }, {
-                    label: '1d',
-                    value: '1440',
-                }, {
-                    label: '2d',
-                    value: '2880',
-                }, {
-                    label: '7d',
-                    value: '10080',
-                })
-            );
+            .setTitle('Modal');
 
         const options = interaction.options.getNumber('options');
 
-        for (let i = 0; i < options; i++) {
-            console.log('Adding option ' + i);
+        for (let i = 1; i >= options; i++) {
             modal.addComponents(
                 new TextInputComponent() // We create a Text Input Component
                 .setCustomId('option' + i)
